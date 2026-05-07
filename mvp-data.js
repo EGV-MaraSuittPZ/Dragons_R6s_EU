f5bc8ac0653b7b234accb00d2d511d0c9a19b148
EGV-MaraSuitt_PZ <egvcij@gmail.com>
2026-05-07 11:33:55 +0200

    Update MVP data for April and March 2026

---
+++ mvp-data.js
@@ -0,0 +1,17 @@
+const mvpData = [
+   {
+    month: "Abril",        
+    year: "2026",          
+    player: "Xx_druiazul", 
+    image: "MVP/05-2026.png",
+    current: true         
+  },
+  {
+    month: "Marzo",        // ← nombre del mes
+    year: "2026",          // ← año
+    player: "qSxntxxx", // ← nick del jugador
+    image: "MVP/04-2026.png", // ← nombre del archivo de imagen
+    current: false          // ← true = MVP actual, false = historial
+  },
+  // ... meses anteriores con current: false
+];
