c22ac7f9236b756d00f1c625eeb6f4971be0e443
EGV-MaraSuitt_PZ <egvcij@gmail.com>
2026-05-08 10:24:42 +0200

    Swap MVP player data for March and April

--- mvp-data.js
+++ mvp-data.js
@@ -1,16 +1,16 @@
 const mvpData = [
    {
-    month: "Abril",        
+    month: "Marzo",        
     year: "2026",          
-    player: "Xx_druiazul", 
-    image: "MVP/05-2026.png",
+    player: "qSxntxxx", 
+    image: "MVP/04-2026.png",
     current: true        
   },
   {
-    month: "Marzo",        // ← nombre del mes
+    month: "Abril",        // ← nombre del mes
     year: "2026",          // ← año
-    player: "qSxntxxx", // ← nick del jugador
-    image: "MVP/04-2026.png", // ← nombre del archivo de imagen
+    player: "Xx_druiazul", // ← nick del jugador
+    image: "MVP/05-2026.png", // ← nombre del archivo de imagen
     current: false          // ← true = MVP actual, false = historial
   },
   // ... meses anteriores con current: false
